6c0fe7ca7010ac0384d5a8e507b5ff9b74250c03
skysnolimit08 <skysnolimit08@gmail.com>
2022-02-09 15:44:28 -0800

    configure using django-heroku package

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from decouple import config
 from pathlib import Path
 from dj_database_url import parse as dburl
@@ -133,3 +134,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
