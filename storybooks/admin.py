41c973367c761e798d45ad6560923c6ab37880bf
skysnolimit08 <skysnolimit08@gmail.com>
2022-02-18 15:37:24 -0800

    trying to configure database

--- storybooks/admin.py
+++ storybooks/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from . import models
+
+
+# @admin.register(models.Post)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'id', 'status', 'slug', 'author')
+#     prepopulated_fields = {'slug': ('title',), }
+
+
+admin.site.register(models.Audio)
+admin.site.register(models.Language)
+admin.site.register(models.Translation)
+admin.site.register(models.Story)
