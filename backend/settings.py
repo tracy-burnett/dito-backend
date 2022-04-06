5df6a88db9a2a5a8fe37448f3fb7ffd6ad3d9767
Jerry Jiao <jerryjiao@berkeley.edu>
2022-04-06 15:25:02 -0700

    commit!

--- backend/settings.py
+++ backend/settings.py
@@ -167,18 +167,18 @@ CORS_ALLOWED_ORIGINS = [
 
 # Firebase setup
 
-# cred = credentials.Certificate({
+cred = credentials.Certificate({
 
-#  "type": os.environ.get('FIREBASE_TYPE'),
-#  "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
-#  "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-#  "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-#  "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
-#  "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
-#  "auth_uri": os.environ.get('FIREBASE_AUTH_URI'),
-#  "token_uri": os.environ.get('FIREBASE_TOKEN_URI'),
-#  "auth_provider_x509_cert_url": os.environ.get('FIREBASE_AUTH_PROVIDER_CERT_URL'),
-#  "client_x509_cert_url": os.environ.get('FIREBASE_CLIENT_CERT_URL'),
+ "type": os.environ.get('FIREBASE_TYPE'),
+ "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
+ "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
+ "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+ "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
+ "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
+ "auth_uri": os.environ.get('FIREBASE_AUTH_URI'),
+ "token_uri": os.environ.get('FIREBASE_TOKEN_URI'),
+ "auth_provider_x509_cert_url": os.environ.get('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+ "client_x509_cert_url": os.environ.get('FIREBASE_CLIENT_CERT_URL'),
 
-# })
-# firebase_admin.initialize_app(cred)
+})
+firebase_admin.initialize_app(cred)
